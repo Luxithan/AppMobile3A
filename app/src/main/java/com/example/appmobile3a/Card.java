66059b1d50ee59599608b70a19d7666b8db0d2a0
Kailairajan Luxithan <kailairajan@et.esiea.fr>
2020-05-22 23:36:30 +0200

    [Feature][Api] API Rest (API Success)

--- app/src/main/java/com/example/appmobile3a/Card.java
+++ app/src/main/java/com/example/appmobile3a/Card.java
@@ -8,11 +8,11 @@ public class Card {
     private Integer nationalPokedexNumber;
     private String imageUrl;
     private String imageUrlHiRes;
-    private List<Type> types;
+//    private List<Type> types;
     private String supertype;
     private String subtype;
     private String hp;          //A changer en Integer si besoin
-    private List<Retreat> retreatCost;
+//    private List<Retreat> retreatCost;
     private Integer convertedRetreatCost;
     private String number;
     private String artist;
@@ -20,9 +20,9 @@ public class Card {
     private String series;
     private String set;
     private String setCode;
-    private List<Attack> attacks;
-    private List<Resistance> resistances;
-    private List<Weakness> weaknesses;
+//    private List<Attack> attacks;
+//    private List<Resistance> resistances;
+//    private List<Weakness> weaknesses;
 
     public String getId() {
         return id;
@@ -43,11 +43,11 @@ public class Card {
     public String getImageUrlHiRes() {
         return imageUrlHiRes;
     }
-
+/*
     public List<Type> getTypes() {
         return types;
     }
-
+*/
     public String getSupertype() {
         return supertype;
     }
@@ -59,11 +59,11 @@ public class Card {
     public String getHp() {
         return hp;
     }
-
+/*
     public List<Retreat> getRetreatCost() {
         return retreatCost;
     }
-
+*/
     public Integer getConvertedRetreatCost() {
         return convertedRetreatCost;
     }
@@ -91,7 +91,7 @@ public class Card {
     public String getSetCode() {
         return setCode;
     }
-
+/*
     public List<Attack> getAttacks() {
         return attacks;
     }
@@ -103,4 +103,6 @@ public class Card {
     public List<Weakness> getWeaknesses() {
         return weaknesses;
     }
+
+ */
 }
--- app/src/main/java/com/example/appmobile3a/MainActivity.java
+++ app/src/main/java/com/example/appmobile3a/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -13,6 +14,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -64,6 +67,25 @@ public class MainActivity extends AppCompatActivity {
         PokeCardApi pokeCardApi = retrofit.create(PokeCardApi.class);
 
         Call<Deck> call = pokeCardApi.getDeck();
-  //      call.enqueue(this);
+        call.enqueue(new Callback<Deck>() {
+            @Override
+            public void onResponse(Call<Deck> call, Response<Deck> response) {
+                if(response.isSuccessful()  &&  response.body() != null){
+                    List<Card> cardList = response.body().getCards();
+                    Toast.makeText(getApplicationContext(), "API Success", Toast.LENGTH_SHORT).show();
+                }else{
+                    showError();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Deck> call, Throwable t) {
+                showError();
+            }
+        });
+    }
+
+    private void showError() {
+        Toast.makeText(getApplicationContext(), "API Error", Toast.LENGTH_SHORT).show();
     }
 }
